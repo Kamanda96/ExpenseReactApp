ffcbd82e3308ea56f198b3d4e4d9cb40fea15d01
Kamanda96 <sullaimankamanda96@gmail.com>
2023-12-07 23:17:52 +0000

    created the expense list

---
+++ frontend/frontend/src/components/ExpenseList.tsx
@@ -0,0 +1,26 @@
+function ExpenseList() {
+  return (
+    <>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default ExpenseList;
